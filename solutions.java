b89db78315aa6ba874501715347296f544b6c405
Sai Manoj Gaddipati <30746079+sai-manoj-gaddipati@users.noreply.github.com>
2024-10-06 02:30:31 -0700

    Update solutions.java

--- solutions.java
+++ solutions.java
@@ -1,4 +1,47 @@
+// Problem #2: 3 Sum
+// Time Complexity:  O(N logN) (sort) + O(N^2 LogN) (loop through list for each number + binary search) --> O(N2 LogN) 
+// Space Complexity: O(1)
+// Algorithm: 2 Sum algorithm for each number where input to 2sum would be target - cur_num 
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        //sort array once
+        int n = nums.length;
+        Arrays.sort(nums);
+        // 2 pointer approach n times
 
+        int target = 0; //in this case
+
+        for (int i = 0; i < n-2; i++){
+            if (i>0 && nums[i] == nums[i-1]) continue; //ie the same number and we dealt with it in prev iteration
+            if (nums[i] > target) break; // since the numbers are sorted and we reached number > target it is 
+                                        //impossible to find a pair of numbers that add up to our target. 
+            int low  = i+1, high = n-1;
+            while(low < high){
+                int sum = nums[i] + nums[low] + nums[high];
+                if(sum == target){
+                    result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    low++;
+                    high--;
+                    while(low < high && nums[low] == nums[low-1]){//skip over duplicate numbers while low < high  
+                                                                //(ie not crossing boundaries)
+                        low++;
+                    }
+                    while(low< high && nums[high] == nums[high+1]){
+                        high--;
+                    }
+                }
+                else if (sum < target){
+                    low++;
+                }
+                else{ // ie. sum > target
+                    high--;
+                }
+            }
+        }
+    return result;
+    }
+}
 // Problem #3: Container with most Water (https://leetcode.com/problems/container-with-most-water/)
 // Time Complexity: O(N)
 // Space Complexity: O(1)
